756ae43106f2ff65b3dc1155016e96cab69eff77
Kristiaan Van den Eynde <magentix@gmail.com>
2015-12-14 15:38:32 +0100

    Sort content enabler plugins by provider.

--- src/Plugin/GroupContentEnablerCollection.php
+++ src/Plugin/GroupContentEnablerCollection.php
@@ -23,4 +23,20 @@ class GroupContentEnablerCollection extends DefaultLazyPluginCollection {
     return parent::get($instance_id);
   }
 
+  /**
+   * {@inheritdoc}
+   *
+   * Sorts plugins by provider.
+   */
+  public function sortHelper($aID, $bID) {
+    $a = $this->get($aID);
+    $b = $this->get($bID);
+
+    if ($a->getProvider() != $b->getProvider()) {
+      return strnatcasecmp($a->getProvider(), $b->getProvider());
+    }
+
+    return parent::sortHelper($aID, $bID);
+  }
+
 }
