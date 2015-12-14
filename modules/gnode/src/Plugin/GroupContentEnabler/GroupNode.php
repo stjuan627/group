1571daa1b70757704c3ffe92c3db6ea98a99d3d8
Kristiaan Van den Eynde <magentix@gmail.com>
2015-12-14 11:10:55 +0100

    Added Group Node submodule basics to test UI.

---
+++ modules/gnode/src/Plugin/GroupContentEnabler/GroupNode.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\gnode\Plugin\GroupContentEnabler\GroupNode.
+ */
+
+namespace Drupal\gnode\Plugin\GroupContentEnabler;
+
+use Drupal\group\Plugin\GroupContentEnablerBase;
+
+/**
+ * Provides a content enabler for nodes.
+ *
+ * @GroupContentEnabler(
+ *   id = "group_node",
+ *   label = @Translation("Group node"),
+ *   description = @Translation("Adds nodes to groups both publicly and privately."),
+ *   entity_type_id = "node",
+ *   deriver = "Drupal\gnode\Plugin\GroupNodeDerivatives"
+ * )
+ */
+class GroupNode extends GroupContentEnablerBase {
+
+}
---
+++ modules/gnode/src/Plugin/GroupNodeDerivatives.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\gnode\Plugin\GroupNodeDerivatives.
+ */
+
+namespace Drupal\gnode\Plugin;
+
+use Drupal\node\Entity\NodeType;
+use Drupal\Component\Plugin\Derivative\DeriverInterface;
+
+class GroupNodeDerivatives implements DeriverInterface {
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getDerivativeDefinition($derivative_id, $base_plugin_definition) {
+    $derivatives = $this->getDerivativeDefinitions($base_plugin_definition);
+    if (isset($derivatives[$derivative_id])) {
+      return $derivatives[$derivative_id];
+    }
+  }
+
+  /**
+   * {@inheritdoc}.
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $derivatives = [];
+
+    foreach (NodeType::loadMultiple() as $name => $node_type) {
+      $label = $node_type->label();
+      $derivatives[$name] = [
+        'label' => t('Group node') . " ($label)",
+        'description' => t('Adds %type content to groups both publicly and privately.', ['%type' => $label])
+      ] + $base_plugin_definition;
+    }
+
+    return $derivatives;
+  }
+
+}
