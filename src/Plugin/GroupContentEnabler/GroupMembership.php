ee6861469f067566630a5f15c10dbaceae683674
Kristiaan Van den Eynde <magentix@gmail.com>
2016-01-13 14:42:14 +0100

    Added a locked entity reference field to group memberships for selecting group roles.

--- src/Plugin/GroupContentEnabler/GroupMembership.php
+++ src/Plugin/GroupContentEnabler/GroupMembership.php
@@ -7,7 +7,12 @@
 
 namespace Drupal\group\Plugin\GroupContentEnabler;
 
+use Drupal\group\Entity\GroupTypeInterface;
 use Drupal\group\Plugin\GroupContentEnablerBase;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
 
 /**
  * Provides a content enabler for users.
@@ -21,5 +26,62 @@ use Drupal\group\Plugin\GroupContentEnablerBase;
  * )
  */
 class GroupMembership extends GroupContentEnablerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postInstall(GroupTypeInterface $group_type) {
+    $group_content_type_id = $this->getContentTypeConfigId($group_type);
+
+    // Add the group_roles field to the newly added group content type.
+    FieldConfig::create([
+      'field_storage' => FieldStorageConfig::loadByName('group_content', 'group_roles'),
+      'bundle' => $group_content_type_id,
+      'label' => $this->t('Roles'),
+      'settings' => [
+        'handler' => 'group_type',
+        'handler_settings' => [
+          'group_type_id' => $group_type->id(),
+        ],
+      ],
+    ])->save();
+
+    // Build the 'default' display ID for both the entity form and view mode.
+    $default_display_id = "group_content.$group_content_type_id.default";
+
+    // Build or retrieve the 'default' form mode.
+    if (!$form_display = EntityFormDisplay::load($default_display_id)) {
+      $form_display = EntityFormDisplay::create([
+        'targetEntityType' => 'group_content',
+        'bundle' => $group_content_type_id,
+        'mode' => 'default',
+        'status' => TRUE,
+      ]);
+    }
+
+    // Build or retrieve the 'default' view mode.
+    if (!$view_display = EntityViewDisplay::load($default_display_id)) {
+      $view_display = EntityViewDisplay::create([
+        'targetEntityType' => 'group_content',
+        'bundle' => $group_content_type_id,
+        'mode' => 'default',
+        'status' => TRUE,
+      ]);
+    }
+
+    // Assign widget settings for the 'default' form mode.
+    $form_display->setComponent('group_roles', [
+      'type' => 'options_buttons',
+    ])->save();
+
+    // Assign display settings for the 'default' view mode.
+    $view_display->setComponent('group_roles', [
+      'label' => 'above',
+      'type' => 'entity_reference_label',
+      'settings' => [
+        'link' => 0,
+      ],
+    ])->save();
+  }
 
 }
