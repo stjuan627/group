db46c96148a521727e00acf73bfce7274ec72eaf
Kristiaan Van den Eynde <magentix@gmail.com>
2017-02-13 18:07:15 +0100

    Issue #2850417 by kristiaanvandeneynde: Remove hook_group_user_roles_alter()

--- src/Entity/Storage/GroupRoleStorage.php
+++ src/Entity/Storage/GroupRoleStorage.php
@@ -22,13 +22,13 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class GroupRoleStorage extends ConfigEntityStorage implements GroupRoleStorageInterface {
 
   /**
-   * Static cache of altered group role IDs.
+   * Static cache of a user's group role IDs.
    *
    * @todo Perhaps we need to be able to clear this cache during runtime?
    *
    * @var array
    */
-  protected $alteredIds = [];
+  protected $userGroupRoleIds = [];
 
   /**
    * The group membership loader.
@@ -44,8 +44,6 @@ class GroupRoleStorage extends ConfigEntityStorage implements GroupRoleStorageIn
    *   The entity type definition.
    * @param \Drupal\group\GroupMembershipLoaderInterface $membership_loader
    *   The group membership loader.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   The module handler.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory service.
    * @param \Drupal\Component\Uuid\UuidInterface $uuid_service
@@ -53,10 +51,9 @@ class GroupRoleStorage extends ConfigEntityStorage implements GroupRoleStorageIn
    * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
    *   The language manager.
    */
-  public function __construct(EntityTypeInterface $entity_type, GroupMembershipLoaderInterface $membership_loader, ModuleHandlerInterface $module_handler, ConfigFactoryInterface $config_factory, UuidInterface $uuid_service, LanguageManagerInterface $language_manager) {
+  public function __construct(EntityTypeInterface $entity_type, GroupMembershipLoaderInterface $membership_loader, ConfigFactoryInterface $config_factory, UuidInterface $uuid_service, LanguageManagerInterface $language_manager) {
     parent::__construct($entity_type, $config_factory, $uuid_service, $language_manager);
     $this->membershipLoader = $membership_loader;
-    $this->moduleHandler = $module_handler;
   }
 
   /**
@@ -66,7 +63,6 @@ class GroupRoleStorage extends ConfigEntityStorage implements GroupRoleStorageIn
     return new static(
       $entity_type,
       $container->get('group.membership_loader'),
-      $container->get('module_handler'),
       $container->get('config.factory'),
       $container->get('uuid'),
       $container->get('language_manager')
@@ -77,7 +73,10 @@ class GroupRoleStorage extends ConfigEntityStorage implements GroupRoleStorageIn
    * {@inheritdoc}
    */
   public function loadByUserAndGroup(AccountInterface $account, GroupInterface $group, $include_implied = TRUE) {
-    if (!isset($this->alteredIds[$account->id()][$group->id()])) {
+    $uid = $account->id();
+    $gid = $group->id();
+
+    if (!isset($this->userGroupRoleIds[$uid][$gid])) {
       $ids = [];
 
       // Get the IDs from the 'group_roles' field, without loading the roles.
@@ -87,29 +86,7 @@ class GroupRoleStorage extends ConfigEntityStorage implements GroupRoleStorageIn
         }
       }
 
-      // Allow modules to alter the list of role IDs the user gets for the group.
-      $this->moduleHandler->alter('group_user_roles', $ids, $account, $group);
-
-      // Run some checks on what people actually tried to set here.
-      foreach ($ids as $key => $id) {
-        list($group_type_id, $role_id) = explode('-', $id . '-');
-
-        // Filter out any roles that are not available to the group's type.
-        if ($group_type_id != $group->bundle()) {
-          unset($ids[$key]);
-        }
-        // Filter out roles that were malformed.
-        elseif (empty($role_id)) {
-          unset($ids[$key]);
-        }
-        // Filter out any special roles someone tried to manually set.
-        elseif (in_array($role_id, ['anonymous', 'outsider', 'member'])) {
-          unset($ids[$key]);
-        }
-      }
-
-      // Only now do we add the implied group role IDs so there's no way someone
-      // could have removed them in their alter hook.
+      // Add the implied group role IDs.
       if ($include_implied) {
         if ($membership !== FALSE) {
           $ids[] = $group->getGroupType()->getMemberRoleId();
@@ -121,10 +98,10 @@ class GroupRoleStorage extends ConfigEntityStorage implements GroupRoleStorageIn
         }
       }
 
-      $this->alteredIds[$account->id()][$group->id()] = $ids;
+      $this->userGroupRoleIds[$uid][$gid] = $ids;
     }
 
-    return $this->loadMultiple($this->alteredIds[$account->id()][$group->id()]);
+    return $this->loadMultiple($this->userGroupRoleIds[$uid][$gid]);
   }
 
 }
