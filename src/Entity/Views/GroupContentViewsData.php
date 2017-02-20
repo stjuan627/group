e7eaa1f34099b1533515487cf2cfcb5fffb17129
renatog <renatog@3326031.no-reply.drupal.org>
2017-02-20 13:45:14 +0100

    Issue #2854163 by renatog: Fixed variable $plugin_id not used and variable $permission_access unnecessary

--- src/Entity/Views/GroupContentViewsData.php
+++ src/Entity/Views/GroupContentViewsData.php
@@ -80,7 +80,7 @@ class GroupContentViewsData extends EntityViewsData {
 
     // Add views data for all defined plugins so modules can provide default
     // views even though their plugins may not have been installed yet.
-    foreach ($this->pluginManager->getAll() as $plugin_id => $plugin) {
+    foreach ($this->pluginManager->getAll() as $plugin) {
       $entity_type_id = $plugin->getEntityTypeId();
       $entity_type = $entity_types[$entity_type_id];
       $entity_data_table = $entity_type->getDataTable() ?: $entity_type->getBaseTable();
