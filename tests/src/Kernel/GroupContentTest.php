11ef18a09c7d2c220a10a485802ffec155068a7e
Kristiaan Van den Eynde <magentix@gmail.com>
2017-11-22 15:29:21 +0100

    Issue #2888588 by kevin.dutra, kristiaanvandeneynde, seanB: Performance issues with node access control

--- tests/src/Kernel/GroupContentTest.php
+++ tests/src/Kernel/GroupContentTest.php
@@ -12,6 +12,11 @@ namespace Drupal\Tests\group\Kernel;
 class GroupContentTest extends GroupKernelTestBase {
 
   /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['group_test'];
+
+  /**
    * Ensure entity url templates are functional.
    *
    * @covers ::urlRouteParameters
@@ -54,6 +59,26 @@ class GroupContentTest extends GroupKernelTestBase {
       $expected = "/group/{$group->id()}/content/{$item->id()}/edit";
       $this->assertEquals($expected, $item->toUrl('edit-form')->toString());
     }
+  }
+
+  /**
+   * Tests that after adding an entity to a group, it gets saved again.
+   *
+   * @covers ::postSave
+   *
+   * @see group_test_user_update()
+   */
+  public function testSubjectResaved() {
+    $changed = 123456789;
+    $account = $this->createUser(['changed' => $changed]);
+
+    $group = $this->createGroup();
+    $group->addContent($account, 'group_membership');
+
+    // All users whose changed time was set to 123456789 get their changed time
+    // set to 530496000 in group_test_user_update() when the account is updated.
+    $account_unchanged = $this->entityTypeManager->getStorage('user')->loadUnchanged($account->id());
+    $this->assertEquals(530496000, $account_unchanged->getChangedTime(), 'Account was saved as part of being added to a group.');
   }
 
 }
