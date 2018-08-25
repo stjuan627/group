2ea1b4d873fc08ff1f15655177619ba8b3216af1
Kristiaan Van den Eynde <kristiaan@factorial.io>
2018-08-25 14:44:19 +0200

    Issue #2995056 by kristiaanvandeneynde: Inject @string_translation in GroupRouteContext

--- src/Context/GroupRouteContext.php
+++ src/Context/GroupRouteContext.php
@@ -8,6 +8,7 @@ use Drupal\Core\Plugin\Context\ContextDefinition;
 use Drupal\Core\Plugin\Context\ContextProviderInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
 
 /**
  * Sets the current group as a context on group routes.
@@ -22,9 +23,12 @@ class GroupRouteContext implements ContextProviderInterface {
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $current_route_match
    *   The current route match object.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
+   *   The string translation service.
    */
-  public function __construct(RouteMatchInterface $current_route_match) {
+  public function __construct(RouteMatchInterface $current_route_match, TranslationInterface $string_translation) {
     $this->currentRouteMatch = $current_route_match;
+    $this->stringTranslation = $string_translation;
   }
 
   /**
