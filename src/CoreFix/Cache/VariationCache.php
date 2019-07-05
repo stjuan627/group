5f2cc981ed97ec795a90c8434dbeb5d881e35a1f
Kristiaan Van den Eynde <magentix@gmail.com>
2019-07-05 11:33:50 +0200

    Issue #3066016 by kristiaanvandeneynde: Update VariationCache to mimic most recent version

--- src/CoreFix/Cache/VariationCache.php
+++ src/CoreFix/Cache/VariationCache.php
@@ -83,31 +83,73 @@ class VariationCache implements VariationCacheInterface {
     // step with a simpler one.
     $chain = $this->getRedirectChain($keys);
     if (!array_key_exists($cid, $chain)) {
-      $redirect = new CacheRedirect($cacheability);
-
       // We can easily find overly specific redirects by comparing their cache
-      // contexts to the ones we have here. If a redirect has more contexts, it
-      // needs to be dumbed down.
+      // contexts to the ones we have here. If a redirect has more or different
+      // contexts, it needs to be replaced with a simplified version.
+      //
+      // Simplifying overly specific redirects can be done in two ways:
+      //
+      // -------
+      //
+      // Problem: The redirect is a superset of the current cache contexts.
+      // Solution: We replace the redirect with the current contexts.
+      //
+      // Example: Suppose we try to store an object with context A, whereas we
+      // already have a redirect that uses A and B. In this case we simply store
+      // the object at the address designated by context A and next time someone
+      // tries to load the initial AB object, it will restore its redirect path
+      // by adding an AB redirect step after A.
+      //
+      // -------
+      //
+      // Problem: The redirect overlaps, with both options having unique values.
+      // Solution: Find the common contexts and use those for a new redirect.
+      //
+      // Example: Suppose we try to store an object with contexts A and C, but
+      // we already have a redirect that uses A and B. In this case we find A to
+      // be the common cache context and replace the redirect with one only
+      // using A, immediately followed by one for AC so there is a full path to
+      // the data we're trying to set. Next time someone tries to load the
+      // initial AB object, it will restore its redirect path by adding an AB
+      // redirect step after A.
       $data_contexts = $cacheability->getCacheContexts();
       foreach ($chain as $chain_cid => $result) {
         if ($result && $result->data instanceof CacheRedirect) {
           $result_contexts = $result->data->getCacheContexts();
           if (array_diff($result_contexts, $data_contexts)) {
+            // Check whether we have an overlap scenario as we need to manually
+            // create an extra redirect in that case.
+            $common_contexts = array_intersect($result_contexts, $data_contexts);
+            if (empty($common_contexts)) {
+              throw new \LogicException('Trying to store a cache redirect with no contexts in common with previously stored cache redirect.');
+            }
+            elseif ($common_contexts != $data_contexts) {
+              // Set the redirect to the common contexts at the current address.
+              // In the above example this is essentially overwriting the
+              // redirect to AB with a redirect to A.
+              $common_cacheability = (new CacheableMetadata())->setCacheContexts($common_contexts);
+              $this->cacheBackend->set($chain_cid, new CacheRedirect($common_cacheability));
+
+              // Before breaking the loop, set the current address to the next
+              // one in line so that we can store the full redirect as well. In
+              // the above example, this is the part where we immediately also
+              // store a redirect to AC at the CID that A pointed to.
+              $chain_cid = $this->createCacheID($keys, $common_cacheability);
+            }
             break;
           }
         }
       }
 
-      // If we couldn't find an overly specific step, we know that the last
-      // cache ID in the chain is where the lookup failed. So we simply add a
-      // redirect with the current cacheability there. Either way, $chain_cid
-      // should now contain the cache ID we're interested in.
+      // The loop above either broke at an overly specific step or completed
+      // without any problem. In both cases, $chain_cid ended up with the value
+      // that we should store the new redirect at.
       //
       // Cache redirects are stored indefinitely and without tags as they never
       // need to be cleared. If they ever end up leading to a stale cache item
       // that now uses different contexts then said item will either follow an
       // existing path of redirects or carve its own over the old one.
-      $this->cacheBackend->set($chain_cid, $redirect);
+      $this->cacheBackend->set($chain_cid, new CacheRedirect($cacheability));
     }
 
     $this->cacheBackend->set($cid, $data, $this->maxAgeToExpire($cacheability->getCacheMaxAge()), $cacheability->getCacheTags());
