ab34f459579604d83492b0ae401174e2343d13fe
Marco von Frieling <mvonfrie@edu.aau.at>
2021-01-07 14:26:06 +0100

    Issue #3118383: Only call postInstall() on the content plugin when config is not imported as it might fail otherwise

--- src/Entity/GroupContentType.php
+++ src/Entity/GroupContentType.php
@@ -209,8 +209,14 @@ class GroupContentType extends ConfigEntityBundleBase implements GroupContentTyp
         \Drupal::service('views.views_data')->clear();
       }
 
-      // Run the post install tasks on the plugin.
-      $this->getContentPlugin()->postInstall();
+      // While installation from existing config (and maybe during "normal" config
+      // import as well) the content plugin might not be available and causing
+      // an exception. As postInstall() only creates config objects while config
+      // import is not in progress, lets only call it when it actually will run.
+      if (!\Drupal::isConfigSyncing()) {
+        // Run the post install tasks on the plugin.
+        $this->getContentPlugin()->postInstall();
+      }
 
       // We need to reset the plugin ID map cache as it will be out of date now.
       $this->getContentEnablerManager()->clearCachedPluginMaps();
