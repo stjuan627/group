2eebd761230d92f4812531d0e7b2f086670ba859
Kristiaan Van den Eynde <magentix@gmail.com>
2021-02-24 16:04:08 +0100

    Wow, this is becoming more fun by the minute. Content translation is SO modifiable from the outside /s In all seriousness: next stop, overriding the ContentTranslationHandler so it actually checks entity access too.

--- src/Access/GroupContentEntityTranslationManageAccess.php
+++ src/Access/GroupContentEntityTranslationManageAccess.php
@@ -2,10 +2,6 @@
 
 namespace Drupal\group\Access;
 
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Language\LanguageInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -17,30 +13,20 @@ use Symfony\Component\Routing\Route;
 class GroupContentEntityTranslationManageAccess implements AccessInterface {
 
   /**
-   * The entity type manager service.
+   * The decorated access check.
    *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   * @var \Drupal\Core\Routing\Access\AccessInterface
    */
-  protected $entityTypeManager;
+  protected $inner;
 
   /**
-   * The language manager service.
+   * Constructs a GroupContentEntityTranslationManageAccess object.
    *
-   * @var \Drupal\Core\Language\LanguageManagerInterface
+   * @param \Drupal\Core\Routing\Access\AccessInterface $inner
+   *   The decorated access check.
    */
-  protected $languageManager;
-
-  /**
-   * Constructs a GroupContentTranslationEntityOverviewAccess object.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager service.
-   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
-   *   The language manager service.
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, LanguageManagerInterface $language_manager) {
-    $this->entityTypeManager = $entity_type_manager;
-    $this->languageManager = $language_manager;
+  public function __construct(AccessInterface $inner) {
+    $this->inner = $inner;
   }
 
   /**
@@ -66,43 +52,20 @@ class GroupContentEntityTranslationManageAccess implements AccessInterface {
    *   The access result.
    */
   public function access(Route $route, RouteMatchInterface $route_match, AccountInterface $account, $source = NULL, $target = NULL, $language = NULL, $entity_type_id = NULL) {
-    if (!$this->languageManager->isMultilingual()) {
-      return AccessResult::neutral();
+    /** @var \Drupal\Core\Access\AccessResultInterface $access */
+    $access = $this->inner->access($route, $route_match, $account, $source, $target, $language, $entity_type_id);
+
+    // If we were to define this access check as a tagged service, the result
+    // would be andIf()ed to the original service's result. In that case, an
+    // AccessResultAllowed would still not grant access if the original service
+    // returned AccessResultNeutral. So we decorate the original and orIf() our
+    // entity access check to that service's result instead.
+    if ($entity = $route_match->getParameter($entity_type_id)) {
+      /* @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+      $access = $access->orIf($entity->access('translate', $account, TRUE));
     }
 
-    /* @var \Drupal\Core\Entity\ContentEntityInterface $entity */
-    $entity = $route_match->getParameter($entity_type_id);
-    if (!$entity || !$entity->isTranslatable() || $entity->getUntranslated()->language()->isLocked()) {
-      return AccessResult::neutral()->addCacheableDependency($entity);
-    }
-
-    $operation = $route->getRequirement('_access_content_translation_manage');
-    if (in_array($operation, ['update', 'delete'])) {
-      // Translation operations cannot be performed on the default translation.
-      if ($language->getId() == $entity->getUntranslated()->language()->getId()) {
-        return AccessResult::forbidden()->addCacheableDependency($entity);
-      }
-    }
-    elseif ($operation == 'create') {
-      $translations = $entity->getTranslationLanguages();
-      $languages = $this->languageManager->getLanguages();
-      $source_language = $this->languageManager->getLanguage($source) ?: $entity->language();
-      $target_language = $this->languageManager->getLanguage($target) ?: $this->languageManager->getCurrentLanguage(LanguageInterface::TYPE_CONTENT);
-
-      $is_new_translation = ($source_language->getId() != $target_language->getId()
-        && isset($languages[$source_language->getId()])
-        && isset($languages[$target_language->getId()])
-        && !isset($translations[$target_language->getId()]));
-
-      if (!$is_new_translation) {
-        return AccessResult::neutral()->addCacheableDependency($entity);
-      }
-    }
-
-    // We know the entity is translatable now, so simply call for "translate"
-    // access and if it is a grouped entity, Group's access layer will kick in
-    // correctly, forbidding access if you do not have the group permission.
-    return $entity->access('translate', $account, TRUE);
+    return $access;
   }
 
 }
--- src/Access/GroupContentEntityTranslationOverviewAccess.php
+++ src/Access/GroupContentEntityTranslationOverviewAccess.php
@@ -2,9 +2,6 @@
 
 namespace Drupal\group\Access;
 
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -15,30 +12,20 @@ use Drupal\Core\Session\AccountInterface;
 class GroupContentEntityTranslationOverviewAccess implements AccessInterface {
 
   /**
-   * The entity type manager service.
+   * The decorated access check.
    *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   * @var \Drupal\Core\Routing\Access\AccessInterface
    */
-  protected $entityTypeManager;
+  protected $inner;
 
   /**
-   * The language manager service.
+   * Constructs a GroupContentEntityTranslationOverviewAccess object.
    *
-   * @var \Drupal\Core\Language\LanguageManagerInterface
+   * @param \Drupal\Core\Routing\Access\AccessInterface $inner
+   *   The decorated access check.
    */
-  protected $languageManager;
-
-  /**
-   * Constructs a GroupContentTranslationEntityOverviewAccess object.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager service.
-   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
-   *   The language manager service.
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, LanguageManagerInterface $language_manager) {
-    $this->entityTypeManager = $entity_type_manager;
-    $this->languageManager = $language_manager;
+  public function __construct(AccessInterface $inner) {
+    $this->inner = $inner;
   }
 
   /**
@@ -55,20 +42,20 @@ class GroupContentEntityTranslationOverviewAccess implements AccessInterface {
    *   The access result.
    */
   public function access(RouteMatchInterface $route_match, AccountInterface $account, $entity_type_id) {
-    if (!$this->languageManager->isMultilingual()) {
-      return AccessResult::neutral();
+    /** @var \Drupal\Core\Access\AccessResultInterface $access */
+    $access = $this->inner->access($route_match, $account, $entity_type_id);
+
+    // If we were to define this access check as a tagged service, the result
+    // would be andIf()ed to the original service's result. In that case, an
+    // AccessResultAllowed would still not grant access if the original service
+    // returned AccessResultNeutral. So we decorate the original and orIf() our
+    // entity access check to that service's result instead.
+    if ($entity = $route_match->getParameter($entity_type_id)) {
+      /* @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+      $access = $access->orIf($entity->access('translate', $account, TRUE));
     }
 
-    /* @var \Drupal\Core\Entity\ContentEntityInterface $entity */
-    $entity = $route_match->getParameter($entity_type_id);
-    if (!$entity || !$entity->isTranslatable() || $entity->getUntranslated()->language()->isLocked()) {
-      return AccessResult::neutral()->addCacheableDependency($entity);
-    }
-
-    // We know the entity is translatable now, so simply call for "translate"
-    // access and if it is a grouped entity, Group's access layer will kick in
-    // correctly, forbidding access if you do not have the group permission.
-    return $entity->access('translate', $account, TRUE);
+    return $access;
   }
 
 }
