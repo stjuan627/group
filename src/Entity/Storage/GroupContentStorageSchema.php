f4bb4f3dc52b6bfeed9fd51d564133b44468f468
Kristiaan Van den Eynde <magentix@gmail.com>
2022-06-08 16:08:18 +0200

    Fix an unused variable.

--- src/Entity/Storage/GroupContentStorageSchema.php
+++ src/Entity/Storage/GroupContentStorageSchema.php
@@ -35,7 +35,7 @@ class GroupContentStorageSchema extends SqlContentEntityStorageSchema {
   protected function getSharedTableFieldSchema(FieldStorageDefinitionInterface $storage_definition, $table_name, array $column_mapping) {
     $schema = parent::getSharedTableFieldSchema($storage_definition, $table_name, $column_mapping);
 
-    if ($table_name = $this->storage->getDataTable()) {
+    if ($this->storage->getDataTable()) {
       $field_name = $storage_definition->getName();
 
       switch ($field_name) {
