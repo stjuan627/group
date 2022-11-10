06dd9fa5cf4f5bb86f6a7e301db7c2739963fb59
Kristiaan Van den Eynde <magentix@gmail.com>
2022-11-10 11:46:01 +0100

    Issue #3311949 by LOBsTerr, kristiaanvandeneynde: GroupRoleStorage doesn't call parent doPreSave

--- src/Entity/Storage/GroupRoleStorage.php
+++ src/Entity/Storage/GroupRoleStorage.php
@@ -94,6 +94,8 @@ class GroupRoleStorage extends ConfigEntityStorage implements GroupRoleStorageIn
     foreach ($violations as $violation) {
       throw new EntityMalformedException($violation->getMessage());
     }
+
+    return parent::doPreSave($entity);
   }
 
   /**
