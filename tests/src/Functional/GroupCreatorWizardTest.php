19393fbe5b9c2b68ebdd9460eb7892478179097c
Ignacio López Flores <ignacio@factorial.io>
2023-09-14 13:28:40 +0200

    Issue #3016197: Test that a group creator just get one membership

--- tests/src/Functional/GroupCreatorWizardTest.php
+++ tests/src/Functional/GroupCreatorWizardTest.php
@@ -41,6 +41,26 @@ class GroupCreatorWizardTest extends GroupBrowserTestBase {
     $submit_button = 'Save group and membership';
     $this->assertSession()->buttonExists($submit_button);
     $this->assertSession()->buttonExists('Back');
+
+    // Submit the form
+    $this->submitForm([], $submit_button);
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Get the group
+    $all_groups = $this->entityTypeManager->getStorage('group')->loadMultiple();
+    $this->assertCount(1, $all_groups);
+    $group = reset($all_groups);
+
+    // Check for the membership.
+    $group_relationship_storage = $this->entityTypeManager->getStorage('group_relationship');
+    $creator_relationships = $group_relationship_storage->loadByEntity($this->groupCreator, 'group_membership');
+
+    // Check the count equals one.
+    $this->assertCount(1, $creator_relationships, 'There is just one membership');
+
+    // Check the belonging group of that membership
+    $creator_relationship = reset($creator_relationships);
+    $this->assertEquals($creator_relationship->getGroupId(), $group->id(), 'Membership belongs to the group');
   }
 
   /**
