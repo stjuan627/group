d5ccff7f8bdf87399827da3d2a2bb9c83e344ef9
Sascha Grossenbacher <saschagros@gmail.com>
2023-09-28 17:19:42 +0200

    Add post update to fix base field override uid default value callback

--- group.post_update.php
+++ group.post_update.php
@@ -4,3 +4,19 @@
  * @file
  * Post update functions for Group.
  */
+
+/**
+ * Updates stale references to Drupal\group\Entity\Context in field overrides.
+ */
+function group_post_update_modify_base_field_author_override() {
+  $uid_fields = \Drupal::entityTypeManager()
+    ->getStorage('base_field_override')
+    ->getQuery()
+    ->condition('entity_type', 'group')
+    ->condition('field_name', 'uid')
+    ->condition('default_value_callback', 'GroupContent', 'CONTAINS')
+    ->execute();
+  foreach (BaseFieldOverride::loadMultiple($uid_fields) as $base_field_override) {
+    $base_field_override->setDefaultValueCallback('Drupal\group\Entity\GroupRelationship::getDefaultEntityOwner')->save();
+  }
+}
