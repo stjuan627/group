ca9f3097ed52b600198da0bc49ae1b469ee16fd6
Kristiaan Van den Eynde <kristiaan@factorial.io>
2024-04-22 10:57:44 +0200

    Add one more test, needs tests still for field instances, view modes, form modes and Views

--- tests/src/Functional/Update/Group2to3UpdateTest.php
+++ tests/src/Functional/Update/Group2to3UpdateTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\group\Tests\Functional\Update;
 
 use Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface;
+use Drupal\field\FieldStorageConfigInterface;
 use Drupal\FunctionalTests\Update\UpdatePathTestBase;
 
 /**
@@ -25,8 +26,7 @@ class Group2to3UpdateTest extends UpdatePathTestBase {
    * Tests that fields referring to group_content are updated correctly.
    */
   public function testEntityReferenceFields(): void {
-    $last_installed_schema_repository = \Drupal::service('entity.last_installed_schema.repository');
-    assert($last_installed_schema_repository instanceof EntityLastInstalledSchemaRepositoryInterface);
+    $last_installed_schema_repository = $this->getLastInstalledSchemaRepository();
 
     $field_storage_definitions = $last_installed_schema_repository->getLastInstalledFieldStorageDefinitions('node');
     $this->assertSame('entity_reference', $field_storage_definitions['field_member_highlight']->getType());
@@ -40,9 +40,36 @@ class Group2to3UpdateTest extends UpdatePathTestBase {
   }
 
   /**
+   * Tests that field storages are updated correctly.
+   */
+  public function testFieldStorages(): void {
+    $last_installed_schema_repository = $this->getLastInstalledSchemaRepository();
+
+    // Make sure no storages exist already.
+    $this->assertEmpty($last_installed_schema_repository->getLastInstalledFieldStorageDefinitions('group_relationship'));
+
+    // Check the control values.
+    $field_storage_definitions = $last_installed_schema_repository->getLastInstalledFieldStorageDefinitions('group_content');
+    assert($field_storage_definitions['field_short_field'] instanceof FieldStorageConfigInterface);
+    $this->assertSame('group_content.field_short_field', $field_storage_definitions['field_short_field']->get('id'));
+    $this->assertSame('group_content', $field_storage_definitions['field_short_field']->get('entity_type'));
+
+    $this->runUpdates();
+
+    // Make sure no storages linger around.
+    $this->assertEmpty($last_installed_schema_repository->getLastInstalledFieldStorageDefinitions('group_content'));
+
+    // Check the new field storage properties.
+    $field_storage_definitions = $last_installed_schema_repository->getLastInstalledFieldStorageDefinitions('group_relationship');
+    assert($field_storage_definitions['field_short_field'] instanceof FieldStorageConfigInterface);
+    $this->assertSame('group_relationship.field_short_field', $field_storage_definitions['field_short_field']->get('id'));
+    $this->assertSame('group_relationship', $field_storage_definitions['field_short_field']->get('entity_type'));
+  }
+
+  /**
    * Tests that the field tables are updated correctly.
    */
-  public function testFieldTableMapping(): void {
+  public function testFieldStorageTableMapping(): void {
     $database = \Drupal::database();
     $database_schema = $database->schema();
 
@@ -95,6 +122,15 @@ class Group2to3UpdateTest extends UpdatePathTestBase {
       'group.relationship_type.class-group_membership',
       'group.relationship_type.class-group_node-page',
     ], \Drupal::configFactory()->listAll('group.relationship_type.'));
+  }
+
+  /**
+   * Gets the last installed schema repository.
+   *
+   * @return \Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface
+   */
+  protected function getLastInstalledSchemaRepository(): EntityLastInstalledSchemaRepositoryInterface {
+    return \Drupal::service('entity.last_installed_schema.repository');
   }
 
 }
